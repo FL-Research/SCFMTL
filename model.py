829d1ed72c00f24dd7e3d6e934b62598000c8a93
pythonloveing <53289613+pythonloveing@users.noreply.github.com>
2022-10-31 11:52:01 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,18 @@
-import crypten.nn as nn
+'''
+Author: ssy
+Date: 2022-10-09 05:03:25
+LastEditors: ssy
+LastEditTime: 2022-10-19 23:06:01
+FilePath: /SCFMTL/model.py
+Description: 
 
+Copyright (c) 2022 by ssy, All Rights Reserved. 
+'''
+
+import crypten.nn as nn
+import torch
+import pretrainedmodels
+import torch.nn.functional as F
 
 class Crypten_Net_mnist(nn.Module):
     def __init__(self):
@@ -32,6 +45,7 @@ class Crypten_Net_cifar(nn.Module):
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
+  
 
     def forward(self, x):
         x = self.pool(self.conv1(x).relu())
@@ -41,3 +55,76 @@ class Crypten_Net_cifar(nn.Module):
         x = self.fc2(x).relu()
         x = self.fc3(x)
         return x.log_softmax(dim=1)
+
+
+class Crypten_Net_caltech101(nn.Module):
+    def __init__(self):
+        super(Crypten_Net_caltech101, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, 5)
+        self.pool = nn.MaxPool2d(5, 5)
+        
+        self.conv2 = nn.Conv2d(64, 128, 3)
+        self.conv3 = nn.Conv2d(128, 512, 3)
+
+        self.fc1 = nn.Linear(512, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 101)
+
+    def forward(self, x):
+        x = self.pool(self.conv1(x).relu())
+        x = self.pool(self.conv2(x).relu())
+        x = self.pool(self.conv3(x).relu())
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x).relu()
+        x = self.fc2(x).relu()
+        x = self.fc3(x)
+        return x.log_softmax(dim=1)
+
+
+
+
+class ResNet34(torch.nn.Module):
+    """resnet34 模型定义
+    """
+    def __init__(self, pretrained):
+        super(ResNet34, self).__init__()
+        if pretrained is True:
+            self.model = pretrainedmodels.__dict__['resnet34'](pretrained='imagenet')
+        else:
+            self.model = pretrainedmodels.__dict__['resnet34'](pretrained=None)
+        
+        # change the classification layer
+        # len(lb.classes_) = 101
+        self.l0 = torch.nn.Linear(512, 101)
+        # self.dropout = torch.nn.Dropout2d(0.4)
+        self.dropout = torch.nn.Dropout(0.4)
+    def forward(self, x):
+        # get the batch size only, ignore (c, h, w)
+        batch, _, _, _ = x.shape
+        x = self.model.features(x)
+        x = F.adaptive_avg_pool2d(x, 1).reshape(batch, -1)
+        x = self.dropout(x)
+        l0 = self.l0(x)
+        return l0
+
+
+
+def net_caltech():
+    """获取caltech数据集所对应的模型
+    """
+    return ResNet34(pretrained=True)
+
+def crypten_net_caltech101():
+    net = net_caltech()
+    dumpy_input = torch.ones(size=[16, 3, 224, 224])
+    crypten_net = nn.from_pytorch(net, dumpy_input)
+
+    return crypten_net
+
+
+def test_crypten_model_load():
+    net = crypten_net_caltech101()
+    net = net.encrypt()
+    net = net.cuda()
+
+
