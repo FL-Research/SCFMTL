09947215a3288aa941f1e9ab5524779317bc92c0
pythonloveing <53289613+pythonloveing@users.noreply.github.com>
2022-10-31 10:14:24 +0800

    Update prox.py

--- CFMTL/prox.py
+++ CFMTL/prox.py
@@ -4,7 +4,7 @@ Date: 2022-10-09 05:03:25
 LastEditors: ssy
 LastEditTime: 2022-10-18 22:46:52
 FilePath: /SCFMTL/CFMTL/prox.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+Description:
 '''
 
 import torch
