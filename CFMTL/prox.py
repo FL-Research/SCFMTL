0478d7fe4ddec8c9875b438f8912114a0394731b
pythonloveing <53289613+pythonloveing@users.noreply.github.com>
2022-10-31 10:01:43 +0800

    Update prox.py
    Put the data on the gpu for calculation to speed up

--- CFMTL/prox.py
+++ CFMTL/prox.py
@@ -1,10 +1,21 @@
-import numpy as np
+'''
+Author: chenfar 825655513@qq.com
+Date: 2022-10-09 05:03:25
+LastEditors: ssy
+LastEditTime: 2022-10-18 22:46:52
+FilePath: /SCFMTL/CFMTL/prox.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+
 import torch
 from model import *
 import copy
 
+from util import cost_time
 
-def L2(old_params, old_w, param, args, rel):
+
+def L2(old_params, old_w, param, args, rel, device):
+    
     w = []
     for p in param:
         w.append(p)
@@ -29,6 +40,7 @@ def L2(old_params, old_w, param, args, rel):
             _old_param = torch.cat([_old_param, old_params[i][j]])
         _old_params.append(_old_param)
     
+
     x = torch.sub(_w, _old_w)
     x = torch.norm(x, 'fro')
     x = torch.pow(x, 2)
@@ -45,32 +57,46 @@ def L2(old_params, old_w, param, args, rel):
         
     return loss
 
-def Prox(w_groups, args, rel):
-    w = L2_Prox(w_groups, args, rel)
+
+
+def Prox(w_groups, args, rel, device):
+    torch.cuda.set_device(1)
+    w = L2_Prox(w_groups, args, rel, device)
     return w
 
-def L2_Prox(w_groups, args, rel):    
+def L2_Prox(w_groups, args, rel, device):    
     if args.dataset == 'mnist':
         Net = Net_mnist
     if args.dataset == 'cifar':
         Net = Net_cifar
+    if args.dataset == "caltech101":
+        Net = net_caltech
     
     old_params = []
+
+    net = Net().cuda()
+    
+
     for w in w_groups:
-        net = Net()
+
         net.load_state_dict(w)
+        
         old_params.append([])
         for p in net.parameters():
             old_params[-1].append(copy.deepcopy(p))
     
     w_n = []
+    
     for i in range(len(w_groups)):
         w = w_groups[i]
-        net = Net()
-        net.load_state_dict(w)        
-        opt = torch.optim.SGD(net.parameters(), lr=args.prox_lr, momentum=args.prox_momentum)
+
+        net.load_state_dict(w)       
+        if args.opt == "adam":
+            opt = torch.optim.Adam(net.parameters(), lr=1e-4)
+        else:
+            opt = torch.optim.SGD(net.parameters(), lr=args.prox_lr, momentum=args.prox_momentum)
         for iter in range(args.prox_local_ep):
-            loss = L2(old_params[:i]+old_params[i+1:], old_params[i], net.parameters(), args, rel[i])
+            loss = L2(old_params[:i]+old_params[i+1:], old_params[i], net.parameters(), args, rel[i], device)
             # if iter == 0 :
             #     loss_start = copy.deepcopy(loss.item())
             # if iter == args.prox_local_ep-1:
